5f74a533a8586ceb8951494f151a594d5094aee1
typowyseba <badscreen32@gmail.com>
2017-03-17 11:01:57 +0100

    Update events.js

--- events.js
+++ events.js
@@ -16,7 +16,7 @@ function click()
 function scroll()
 {
     //document.body.innerHTML += '<div class="block">x</div>';
-    body.innerHTML += '<div class="block">x</div>';
+    document.body.innerHTML += '<div class="block">x</div>';
     //body.innerHTML += '<div style="width: 100px; height: 100px; background: red;">block</div>';
     scrollCount ++;
 }
